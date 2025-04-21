80286f57a74ef1a717c95e4ecbbad5ee4d94e1a5
sumeyahussein17 <sumeyahussein21@gmail.com>
2025-04-21 19:29:07 +0300

    done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React from 'react';
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, technologies }) {
   return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+    <div style={{ marginBottom: '1rem' }}>
+      <h2>{name}</h2>
+      <div>
+        {technologies.map((tech, index) => (
+          <span
+            key={index}
+            style={{
+              display: 'inline-block',
+              padding: '0.25rem 0.5rem',
+              margin: '0 0.25rem 0.25rem 0',
+              backgroundColor: '#f0f0f0',
+              borderRadius: '5px',
+              fontSize: '0.85rem'
+            }}
+          >
+            {tech}
+          </span>
+        ))}
       </div>
     </div>
   );
